b19629a31a7ce66162c7f00ba900e2b3d59cba82
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-11 14:24:38 -0800

    adds the button for quiz

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions and grab DOM elements
-
+const skateButton = document.getElementById('skate-button');
+const skateResults = document.getElementById('skate-results');
 // initialize state
+
+skateButton.addEventListener('click', function(){
+    
+});
+    
+  
 
 // set event listeners to update state and DOM
