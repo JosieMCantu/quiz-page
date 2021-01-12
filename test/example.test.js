794dc50976d9fbb09b49f778e14bf8e98d078895
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-12 08:51:01 -0800

    adds the testing portion

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,31 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { answerYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('if user enters y they get true', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = answerYes('y');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('if user enters no they get false', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = answerYes('no');
 
     //Expect
     // Make assertions about what is expected versus the actual result
